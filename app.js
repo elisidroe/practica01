69feac39dd7fc2bf9a921c0082e7a6f05a17d773
elisidore <celc04lerdo@gmail.com>
2024-03-13 09:07:49 -0600

    New branch

---
+++ app.js
@@ -0,0 +1,26 @@
+var num1 = 79898;
+var num2 = 68986;
+var nombre = "Elias";
+console.log("Hola mi nombre es:", nombre, "y he realizado estas operaciones matematicas")
+
+ var suma = num1 + num2;
+ console.log( "La suma del resultado es:" , suma);
+
+ var resta = num1 - num2;
+ console.log("La resta es:",resta);
+
+ var multiplicacion = num1 * num2;
+ console.log("La multiplicacion es:", multiplicacion);
+
+ var division = num1 / num2;
+ console.log("La division es:", division);
+
+// 7 8 6 sacar promedio de las sig cal
+var num3 = 7;
+var num4 = 8;
+var num5 = 5;
+var promedio = (num3 + num4 + num5 )/3;
+console.log ("La calificaion de Julio fue:",promedio)
+
+
+
